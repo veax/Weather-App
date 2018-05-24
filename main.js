d4ae0c491a94e46310df712ece9b8ec853142e32
veax <borissowadim@mail.ru>
2018-05-24 21:07:19 +0200

    switch celsius/farenheit added

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
 // 1. Find the geolocation of user
 // 2. Request GET api with this location
 // 3. Print information on screen and change image in function of data
+
 $(document).ready(function(){
-    // 1.
+    //getLocation(); 
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(getWeather);
+    }
+    else{
+        console.log("Geolocation not supported");
+    }
     function getWeather(position){
-        console.log("Latitude:" + position.coords.latitude);
-        console.log("Longitude:" + position.coords.longitude);
         var lat = position.coords.latitude;
         var long = position.coords.longitude;
-        var url = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long; 
-        $.getJSON(url, function(data){
-            console.log(data);
-            $("#city").html('<strong>' + data.name + '</strong> (' + data.sys.country + ')');
-            $("#temp").html('<strong>' + data.main.temp + ' C</strong>');
-            $("p.lead").html(data.weather[0].description);
-            $("#icon").prepend('<img id="weather_icon" src=' + data.weather[0].icon + '/>');
+        $.ajax({
+            url: 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long,
+            async: false,
+            dataType: 'json',
+            success: function(data){
+                fht = data.main.temp*9/5 + 32;
+                cels = data.main.temp;
+                $("#city").html('<strong>' + data.name + '</strong> (' + data.sys.country + ')');
+                $("#temp").html('<strong>' + data.main.temp + ' C</strong>');
+                $("p.lead").html(data.weather[0].description);
+                $("#icon").prepend('<img id="weather_icon" src=' + data.weather[0].icon + '/>');
+            }
+        });
+    
+        $("input[type='checkbox']").click(function(){
+            if (this.checked){
+                //change celsius in farenheit 
+                $("#temp").html('<strong>' + fht + ' F</strong>');
+                $("html").addClass("addedclass");
+                $("body").addClass("addedclass");
+            }
+            else{
+                //change farenheit in celsius
+                $("#temp").html('<strong>' + cels + ' C</strong>');
+                $("html").removeClass("addedclass");
+                $("body").removeClass("addedclass");
+            }
         });
     }
-    function getLocation(){
-        if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(getWeather);
-        }
-        else{
-            console.log("Geolocation not supported");
-        }
-    }
-    console.log("156");
-    getLocation();
-
 });
